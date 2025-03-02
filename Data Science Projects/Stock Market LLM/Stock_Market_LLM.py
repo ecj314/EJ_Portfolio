b42ea810bc86dfe362cc188d8d6c0bb578137bb1
Ellis <elliscjones@outlook.com>
2025-03-02 15:27:52 +0000

    Amended File Structure

---
+++ Data Science Projects/Stock Market LLM/Stock_Market_LLM.py
@@ -0,0 +1,15 @@
+import openai
+client = openai.OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "developer", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
