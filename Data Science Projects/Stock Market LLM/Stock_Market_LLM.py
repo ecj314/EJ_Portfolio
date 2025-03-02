79ce564bd0396121f6ce784ff284b884a5ff6b16
Ellis <elliscjones@outlook.com>
2025-03-02 15:51:16 +0000

    Amended File Structure

--- Data Science Projects/Stock Market LLM/Stock_Market_LLM.py
+++ Data Science Projects/Stock Market LLM/Stock_Market_LLM.py
@@ -1,5 +1,4 @@
 import openai
-client = openai.OpenAI(api_key="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")
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
